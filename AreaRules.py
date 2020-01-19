ce7ace2d1d83f5695dd3a771f2e179d97052ff0d
BV-Scrapy <cafebeanant@gmail.com>
2020-01-19 14:10:23 +0000

    controlButtonsArea draft

--- AreaRules.py
+++ AreaRules.py
@@ -31,12 +31,16 @@ class playbackWindow:
 	# define sub areas
 	albumArtArea = area()
 
-	controlZone = area()
+	controlButtonsArea = area()
 
-	metadataZone = area()
-	titleZone = area()
-	albumZone = area()
-	artistZone = area()
+	metadataArea = area()
+	titleTextArea = area()
+	albumTextArea = area()
+	artistTextArea = area()
+
+	playButtonArea = area()
+	nextButtonArea = area()
+	previousButtonArea = area()
 
 def landscapeZones(window):
 	# define main areas
@@ -106,56 +110,85 @@ def landscapeZones(window):
 		window.albumArtArea.height = AlbumArtSize
 		window.albumArtArea.top = int((window.artArea.height - window.albumArtArea.width) / 2)
 
-	# controlZone
-	window.controlZone.top = window.controlArea.top + int (window.controlArea.height / 2) - 50 # assuming height of controls is 100
-	window.controlZone.width = window.controlArea.width - (generalMargin * 2)
-	if window.controlZone.width > window.albumArtArea.width:
-		window.controlZone.width = window.albumArtArea.width
-	window.controlZone.left = window.width - generalMargin - window.controlZone.width
-	window.controlZone.height = 100
+	# controlButtonsArea
+	# this entire section is based off the wrong elements - it should be based on an overall ratio to ensure logical minimum/maximum sizes and smooth transitions
 
-	# titleZone, albumZone and artistZone vertical positioning is relative
+	window.controlButtonsArea.top = window.controlArea.top + int (window.controlArea.height / 2) - 50 # assuming height of controls is 100
+	window.controlButtonsArea.width = window.controlArea.width - (generalMargin * 2)
+	if window.controlButtonsArea.width > window.albumArtArea.width:
+		window.controlButtonsArea.width = window.albumArtArea.width
+	window.controlButtonsArea.left = window.controlArea.left + int(window.controlArea.width / 2) - (window.controlButtonsArea.width / 2)
+	window.controlButtonsArea.height = 100
+
+	# define control buttons
+	# playButtonArea
+	window.playButtonArea.left = window.controlButtonsArea.left + int(window.controlButtonsArea.width / 2) - 50
+	window.playButtonArea.top = window.controlButtonsArea.top
+	window.playButtonArea.width = window.controlButtonsArea.height
+	window.playButtonArea.height = window.controlButtonsArea.height
+
+	distanceModifier = 50
+
+	# alt method
+	# align nextButtonArea.left with window.controlButtonsArea.left + distanceModifier
+	# align previous ButtonArea.left with window.controlButtonsArea.left + window.controlButtonsArea.width - distanceModifier - window.previousButtonArea.width
+	# apply maximum width algorithm to window.controlButtonsArea.left and window.controlButtonsArea.width
+
+	# nextButtonArea
+	window.nextButtonArea.width = 80
+	window.nextButtonArea.height = 80
+	window.nextButtonArea.left = window.controlButtonsArea.left + distanceModifier
+	window.nextButtonArea.top = window.controlButtonsArea.top + int((window.controlButtonsArea.height - window.nextButtonArea.height) / 2)
+
+	# previousButtonArea
+	window.previousButtonArea.width = 80
+	window.previousButtonArea.height = 80
+	window.previousButtonArea.left = window.controlButtonsArea.left + window.controlButtonsArea.width - distanceModifier - window.previousButtonArea.width
+	window.previousButtonArea.top = window.controlButtonsArea.top + int((window.controlButtonsArea.height - window.previousButtonArea.height) / 2)
+
+
+	# titleTextArea, albumTextArea and artistTextArea vertical positioning is relative
 	# lower limit is window.controlArea.height - general margin
-	# upper limit is window.controlZone.top + window.ControlZone.height + generalMargin
+	# upper limit is window.controlButtonsArea.top + window.controlButtonsArea.height + generalMargin
 	
-	# metadataZone
-	gapBeneathControlZone = (window.barArea.top - generalMargin) - (window.controlZone.top + window.controlZone.height)
-	window.metadataZone.left = window.controlArea.left + generalMargin
-	window.metadataZone.top = window.controlZone.top + window.controlZone.height + int(gapBeneathControlZone * 0.4)
-	window.metadataZone.width = window.controlArea.width - (generalMargin *2)
-	window.metadataZone.height = window.controlArea.height - generalMargin - window.metadataZone.top
+	# metadataArea
+	gapBeneathcontrolButtonsArea = (window.barArea.top - generalMargin) - (window.controlButtonsArea.top + window.controlButtonsArea.height)
+	window.metadataArea.left = window.controlArea.left + generalMargin
+	window.metadataArea.top = window.controlButtonsArea.top + window.controlButtonsArea.height + int(gapBeneathcontrolButtonsArea * 0.4)
+	window.metadataArea.width = window.controlArea.width - (generalMargin *2)
+	window.metadataArea.height = window.controlArea.height - generalMargin - window.metadataArea.top
 
-	# metadataZone is split into 5 vertical sections
-	# uppermost (titleZone) is 24% of metedataZone.height
-	# all others (albumZone, artistZone and gaps between them all) are 19% of metadataZone.height
+	# metadataArea is split into 5 vertical sections
+	# uppermost (titleTextArea) is 24% of metedataZone.height
+	# all others (albumTextArea, artistTextArea and gaps between them all) are 19% of metadataArea.height
 
 	titleHeightPC = 24
-	titleHeightPX = int((window.metadataZone.height / 100) * titleHeightPC)
+	titleHeightPX = int((window.metadataArea.height / 100) * titleHeightPC)
 	otherHeightPC = (100 - titleHeightPC) / 4
-	otherHeightPX = int((window.metadataZone.height / 100) * otherHeightPC)
+	otherHeightPX = int((window.metadataArea.height / 100) * otherHeightPC)
 
 	print('titleHeightPC: ', titleHeightPC)
 	print('titleHeightPX: ', titleHeightPX)
 	print('otherHeightPC: ', otherHeightPC)
 	print('otherHeightPX: ', otherHeightPX)
 
-	# titleZone
-	window.titleZone.left = window.metadataZone.left
-	window.titleZone.top = window.metadataZone.top
-	window.titleZone.width = window.metadataZone.width
-	window.titleZone.height = titleHeightPX
+	# titleTextArea
+	window.titleTextArea.left = window.metadataArea.left
+	window.titleTextArea.top = window.metadataArea.top
+	window.titleTextArea.width = window.metadataArea.width
+	window.titleTextArea.height = titleHeightPX
 
-	# albumZone
-	window.albumZone.left = window.metadataZone.left
-	window.albumZone.top = window.metadataZone.top + window.metadataZone.height - (otherHeightPX * 3)
-	window.albumZone.width = window.metadataZone.width
-	window.albumZone.height = otherHeightPX
+	# albumTextArea
+	window.albumTextArea.left = window.metadataArea.left
+	window.albumTextArea.top = window.metadataArea.top + window.metadataArea.height - (otherHeightPX * 3)
+	window.albumTextArea.width = window.metadataArea.width
+	window.albumTextArea.height = otherHeightPX
 
-	# artistZone
-	window.artistZone.left = window.metadataZone.left
-	window.artistZone.top = window.metadataZone.top + window.metadataZone.height - otherHeightPX
-	window.artistZone.width = window.metadataZone.width
-	window.artistZone.height = otherHeightPX
+	# artistTextArea
+	window.artistTextArea.left = window.metadataArea.left
+	window.artistTextArea.top = window.metadataArea.top + window.metadataArea.height - otherHeightPX
+	window.artistTextArea.width = window.metadataArea.width
+	window.artistTextArea.height = otherHeightPX
 
 def resizeMainPanel(event):
 	global window
@@ -169,18 +202,24 @@ def resizeMainPanel(event):
 
 	print('Window: ', window.top, window.left, window.width, window.height)
 
-	print('metadataZone top: ', window.metadataZone.top)
-	print('metadataZone height: ', window.metadataZone.height)
+	print('metadataArea top: ', window.metadataArea.top)
+	print('metadataArea height: ', window.metadataArea.height)
 
 	windowCanvas.coords(barAreaRectangle, window.barArea.left, window.barArea.top, window.barArea.left + window.barArea.width, window.barArea.top + window.barArea.height)
+
 	windowCanvas.coords(artAreaRectangle, window.artArea.left, window.artArea.top, window.artArea.left + window.artArea.width, window.artArea.top + window.artArea.height)
-	windowCanvas.coords(controlAreaRectangle, window.controlArea.left, window.controlArea.top, window.controlArea.left + window.controlArea.width, window.controlArea.top + window.controlArea.height)
 	windowCanvas.coords(albumArtAreaRectangle, window.albumArtArea.left, window.albumArtArea.top, window.albumArtArea.left + window.albumArtArea.width, window.albumArtArea.top + window.albumArtArea.height)
-	windowCanvas.coords(controlZoneRectangle, window.controlZone.left, window.controlZone.top, window.controlZone.left + window.controlZone.width, window.controlZone.top + window.controlZone.height)
-	windowCanvas.coords(metadataZoneRectangle, window.metadataZone.left, window.metadataZone.top, window.metadataZone.left + window.metadataZone.width, window.metadataZone.top + window.metadataZone.height)
-	windowCanvas.coords(titleZoneRectangle, window.titleZone.left, window.titleZone.top, window.titleZone.left + window.titleZone.width, window.titleZone.top + window.titleZone.height)
-	windowCanvas.coords(albumZoneRectangle, window.albumZone.left, window.albumZone.top, window.albumZone.left + window.albumZone.width, window.albumZone.top + window.albumZone.height)
-	windowCanvas.coords(artistZoneRectangle,window.artistZone.left, window.artistZone.top, window.artistZone.left + window.artistZone.width, window.artistZone.top + window.artistZone.height)
+
+	windowCanvas.coords(controlAreaRectangle, window.controlArea.left, window.controlArea.top, window.controlArea.left + window.controlArea.width, window.controlArea.top + window.controlArea.height)
+	windowCanvas.coords(controlButtonsAreaRectangle, window.controlButtonsArea.left, window.controlButtonsArea.top, window.controlButtonsArea.left + window.controlButtonsArea.width, window.controlButtonsArea.top + window.controlButtonsArea.height)
+	windowCanvas.coords(playButtonAreaRectangle, window.playButtonArea.left, window.playButtonArea.top, window.playButtonArea.left + window.playButtonArea.width, window.playButtonArea.top + window.playButtonArea.height)
+	windowCanvas.coords(nextButtonAreaRectangle, window.nextButtonArea.left, window.nextButtonArea.top, window.nextButtonArea.left + window.nextButtonArea.width, window.nextButtonArea.top + window.nextButtonArea.height)
+	windowCanvas.coords(previousButtonAreaRectangle, window.previousButtonArea.left, window.previousButtonArea.top, window.previousButtonArea.left + window.previousButtonArea.width, window.previousButtonArea.top + window.previousButtonArea.height)
+
+	windowCanvas.coords(metadataAreaRectangle, window.metadataArea.left, window.metadataArea.top, window.metadataArea.left + window.metadataArea.width, window.metadataArea.top + window.metadataArea.height)
+	windowCanvas.coords(titleTextAreaRectangle, window.titleTextArea.left, window.titleTextArea.top, window.titleTextArea.left + window.titleTextArea.width, window.titleTextArea.top + window.titleTextArea.height)
+	windowCanvas.coords(albumTextAreaRectangle, window.albumTextArea.left, window.albumTextArea.top, window.albumTextArea.left + window.albumTextArea.width, window.albumTextArea.top + window.albumTextArea.height)
+	windowCanvas.coords(artistTextAreaRectangle,window.artistTextArea.left, window.artistTextArea.top, window.artistTextArea.left + window.artistTextArea.width, window.artistTextArea.top + window.artistTextArea.height)
 
 # declare window
 window = playbackWindow()
@@ -213,31 +252,44 @@ windowCanvas = Canvas(mainPanel, width = window.width, height = window.height, b
 windowCanvas.pack()
 
 # barArea rectangle
-barAreaRectangle = windowCanvas.create_rectangle(window.barArea.left, window.barArea.top, window.barArea.left + window.barArea.width, window.barArea.top + window.barArea.height, width=0, fill="blue")
+# barAreaRectangle = windowCanvas.create_rectangle(window.barArea.left, window.barArea.top, window.barArea.left + window.barArea.width, window.barArea.top + window.barArea.height, width=0, fill="blue")
+barAreaRectangle = windowCanvas.create_rectangle(window.barArea.left, window.barArea.top, window.barArea.left + window.barArea.width, window.barArea.top + window.barArea.height, width=0, fill="#111111")
 
 # artArea rectangle
-artAreaRectangle = windowCanvas.create_rectangle(window.artArea.left, window.artArea.top, window.artArea.left + window.artArea.width, window.artArea.top + window.artArea.height, width=0, fill="red")
+# artAreaRectangle = windowCanvas.create_rectangle(window.artArea.left, window.artArea.top, window.artArea.left + window.artArea.width, window.artArea.top + window.artArea.height, width=0, fill="red")
+artAreaRectangle = windowCanvas.create_rectangle(window.artArea.left, window.artArea.top, window.artArea.left + window.artArea.width, window.artArea.top + window.artArea.height, width=0, fill="#222222")
 
 # controlArea rectangle
-controlAreaRectangle = windowCanvas.create_rectangle(window.controlArea.left, window.controlArea.top, window.controlArea.left + window.controlArea.width, window.controlArea.top + window.controlArea.height, width=0, fill="yellow")
+# controlAreaRectangle = windowCanvas.create_rectangle(window.controlArea.left, window.controlArea.top, window.controlArea.left + window.controlArea.width, window.controlArea.top + window.controlArea.height, width=0, fill="yellow")
+controlAreaRectangle = windowCanvas.create_rectangle(window.controlArea.left, window.controlArea.top, window.controlArea.left + window.controlArea.width, window.controlArea.top + window.controlArea.height, width=0, fill="#222222")
 
 # albumArtArea rectangle
 albumArtAreaRectangle = windowCanvas.create_rectangle(window.albumArtArea.left, window.albumArtArea.top, window.albumArtArea.left + window.albumArtArea.width, window.albumArtArea.top + window.albumArtArea.height, width=0, fill="green")
 
-# controlZone rectangle
-controlZoneRectangle = windowCanvas.create_rectangle(window.controlZone.left, window.controlZone.top, window.controlZone.left + window.controlZone.width, window.controlZone.top + window.controlZone.height, width=0, fill="white")
+# controlButtonsArea rectangle
+controlButtonsAreaRectangle = windowCanvas.create_rectangle(window.controlButtonsArea.left, window.controlButtonsArea.top, window.controlButtonsArea.left + window.controlButtonsArea.width, window.controlButtonsArea.top + window.controlButtonsArea.height, width=0, fill="gray")
 
-# metadataZone rectangle
-metadataZoneRectangle = windowCanvas.create_rectangle(window.metadataZone.left, window.metadataZone.top, window.metadataZone.left + window.metadataZone.width, window.metadataZone.top + window.metadataZone.height, width=0, fill="orange")
+# playButtonArea
+playButtonAreaRectangle = windowCanvas.create_rectangle(window.playButtonArea.left, window.playButtonArea.top, window.playButtonArea.left + window.playButtonArea.width, window.playButtonArea.top + window.playButtonArea.height, width=0, fill="white")
 
-# titleZone rectangle
-titleZoneRectangle = windowCanvas.create_rectangle(window.titleZone.left, window.titleZone.top, window.titleZone.left + window.titleZone.width, window.titleZone.top + window.titleZone.height, width=0, fill="white")
+# nextButtonArea
+nextButtonAreaRectangle = windowCanvas.create_rectangle(window.nextButtonArea.left, window.nextButtonArea.top, window.nextButtonArea.left + window.nextButtonArea.width, window.nextButtonArea.top + window.nextButtonArea.height, width=0, fill="white")
 
-# albumZone rectangle
-albumZoneRectangle = windowCanvas.create_rectangle(window.albumZone.left, window.albumZone.top, window.albumZone.left + window.albumZone.width, window.albumZone.top + window.albumZone.height, width=0, fill="white")
+# previousButtonArea
+previousButtonAreaRectangle = windowCanvas.create_rectangle(window.previousButtonArea.left, window.previousButtonArea.top, window.previousButtonArea.left + window.previousButtonArea.width, window.previousButtonArea.top + window.previousButtonArea.height, width=0, fill="white")
 
-# artistZone rectangle
-artistZoneRectangle = windowCanvas.create_rectangle(window.artistZone.left, window.artistZone.top, window.artistZone.left + window.artistZone.width, window.artistZone.top + window.artistZone.height, width=0, fill="white")
+# metadataArea rectangle
+# metadataAreaRectangle = windowCanvas.create_rectangle(window.metadataArea.left, window.metadataArea.top, window.metadataArea.left + window.metadataArea.width, window.metadataArea.top + window.metadataArea.height, width=0, fill="orange")
+metadataAreaRectangle = windowCanvas.create_rectangle(window.metadataArea.left, window.metadataArea.top, window.metadataArea.left + window.metadataArea.width, window.metadataArea.top + window.metadataArea.height, width=0)
+
+# titleTextArea rectangle
+titleTextAreaRectangle = windowCanvas.create_rectangle(window.titleTextArea.left, window.titleTextArea.top, window.titleTextArea.left + window.titleTextArea.width, window.titleTextArea.top + window.titleTextArea.height, width=0, fill="white")
+
+# albumTextArea rectangle
+albumTextAreaRectangle = windowCanvas.create_rectangle(window.albumTextArea.left, window.albumTextArea.top, window.albumTextArea.left + window.albumTextArea.width, window.albumTextArea.top + window.albumTextArea.height, width=0, fill="white")
+
+# artistTextArea rectangle
+artistTextAreaRectangle = windowCanvas.create_rectangle(window.artistTextArea.left, window.artistTextArea.top, window.artistTextArea.left + window.artistTextArea.width, window.artistTextArea.top + window.artistTextArea.height, width=0, fill="white")
 
 mainPanel.bind( "<Configure>", resizeMainPanel)
 
